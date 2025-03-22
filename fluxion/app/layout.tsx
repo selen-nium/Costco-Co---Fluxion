0cc4ad29894ca11349bf99a62d045a3c3b1b5e9c
keechongwei <keechongwei@gmail.com>
2025-03-23 02:47:52 +0800

    edited Layout to prevent Hydration issue

--- fluxion/app/layout.tsx
+++ fluxion/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-screen">
+      <body className="h-screen vsc-initialized">
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
